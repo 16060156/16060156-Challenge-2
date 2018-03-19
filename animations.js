09283c1635ab691c6df6f2ed71e176077b61d1d0
Laurence van Es <Laurence@mbpvanlaurence.wireless.hhs.nl>
2018-03-19 14:37:33 +0100

    sun

--- animations.js
+++ animations.js
@@ -4,37 +4,45 @@ function maintime() {
     var hours = d.getHours();
     var mins = d.getMinutes();
     
-    var weekday = new Array(7);
-        weekday[0] = "Sunday";
-        weekday[1] = "Monday";
-        weekday[2] = "Tuesday";
-        weekday[3] = "Wednesday";
-        weekday[4] = "Thursday";
-        weekday[5] = "Friday";
-        weekday[6] = "Saturday";
+    if (mins < 10) {
+        mins = "0"+mins;
+    }
+    
+    var weekDay = new Array(7);
+        weekDay[0] = "Sunday";
+        weekDay[1] = "Monday";
+        weekDay[2] = "Tuesday";
+        weekDay[3] = "Wednesday";
+        weekDay[4] = "Thursday";
+        weekDay[5] = "Friday";
+        weekDay[6] = "Saturday";
 
-    var day = weekday[d.getDay()];
+    var day = weekDay[d.getDay()];
     var dayNumber = d.getDate();
     
-    var monthname = new Array(12);
-        weekday[0] = "january";
-        weekday[1] = "february";
-        weekday[2] = "march";
-        weekday[3] = "april";
-        weekday[4] = "may";
-        weekday[5] = "june";
-        weekday[6] = "july";
-        weekday[7] = "august";
-        weekday[8] = "september";
-        weekday[9] = "october";
-        weekday[10] = "november";
-        weekday[11] = "december";
+    var monthName = new Array(12);
+        monthName[0] = "january";
+        monthName[1] = "february";
+        monthName[2] = "march";
+        monthName[3] = "april";
+        monthName[4] = "may";
+        monthName[5] = "june";
+        monthName[6] = "july";
+        monthName[7] = "august";
+        monthName[8] = "september";
+        monthName[9] = "october";
+        monthName[10] = "november";
+        monthName[11] = "december";
     
-    var month = monthname[d.getMonth()];
+    var month = monthName[d.getMonth()];
     
     document.getElementById("mainTime").innerHTML = hours+":"+mins;
     document.getElementById("mainDay").innerHTML = day+", "+dayNumber+" "+month;
     
+   if (hours >= 0 && hours <= 23) {
+           document.getElementById('background').style.background = '-webkit-linear-gradient('
+        + 'rgba(4, 24, 35, 0.80)' + ', ' + 'rgba(18, 9, 40, 0.80)' + '), url(alleen voor background)';   
+       }
 }
 
 setInterval(maintime, 100);
