f1ab5feee693751f7b42771e15a8a3335afc1e52
Laurence van Es <Laurence@mbpvanlaurence.wireless.hhs.nl>
2018-03-19 12:50:44 +0100

    - month

--- animations.js
+++ animations.js
@@ -5,7 +5,7 @@ function maintime() {
     var mins = d.getMinutes();
     
     var weekday = new Array(7);
-        weekday[0] =  "Sunday";
+        weekday[0] = "Sunday";
         weekday[1] = "Monday";
         weekday[2] = "Tuesday";
         weekday[3] = "Wednesday";
@@ -14,8 +14,26 @@ function maintime() {
         weekday[6] = "Saturday";
 
     var day = weekday[d.getDay()];
+    var dayNumber = d.getDate();
     
-    document.getElementById("mainTime").innerHTML = hours+":"+mins+" "+day;
+    var monthname = new Array(12);
+        weekday[0] = "january";
+        weekday[1] = "february";
+        weekday[2] = "march";
+        weekday[3] = "april";
+        weekday[4] = "may";
+        weekday[5] = "june";
+        weekday[6] = "july";
+        weekday[7] = "august";
+        weekday[8] = "september";
+        weekday[9] = "october";
+        weekday[10] = "november";
+        weekday[11] = "december";
+    
+    var month = monthname[d.getMonth()];
+    
+    document.getElementById("mainTime").innerHTML = hours+":"+mins;
+    document.getElementById("mainDay").innerHTML = day+", "+dayNumber+" "+month;
     
 }
 
